cafa541efa2565db12bc40a6070c31e268244cf9
Hyles <Steven.Hyles@Johncrane.com>
2017-01-18 13:04:49 +0000

    Added basic list and editing screen for devices

--- FirstDemo/controllers/devices.py
+++ FirstDemo/controllers/devices.py
@@ -2,4 +2,13 @@
 # try something like
 @auth.requires_login()
 def index(): 
-    return dict(message="hello from devices.py")
+    devices = db(db.device).select()
+    message= "List Method"    
+    return locals()
+
+#@auth.requires_membership('Device Configuration')
+def edit():
+    form = SQLFORM(db.device).process()
+    form2=SQLFORM.factory(Field('deviceName')).process()
+    deviceId = request.args[0]
+    return locals()
---
+++ FirstDemo/models/devices.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+db.define_table('device',
+          Field('DeviceName', 'string'),
+          Field('Ip', 'string'),
+          Field('Port', 'string')
+          )
